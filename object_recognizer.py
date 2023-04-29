cb224262b7a2e2616e7d00bc28e7116ed6f83c78
Natanael-Mota15 <108821447+Natanael-Mota15@users.noreply.github.com>
2023-04-29 15:25:15 -0400

    code bad

--- object_recognizer.py
+++ object_recognizer.py
@@ -18,7 +18,6 @@ print("starting object recognizer...")
 object_to_find = 0
 
 def get_object_from_cloud(image_path):
-
     with io.open(image_path, 'rb') as image_file:
         content = image_file.read()
 
@@ -26,7 +25,7 @@ def get_object_from_cloud(image_path):
     response = client.label_detection(image=image) #full response from Google cloud vision
     label_annotations = response.label_annotations #array of objects labeled
     #print(label_annotations)
-    description = label_annotations[0].description #get description of one object, the first being the one with the highest confidnce score.
+    description = label_annotations[0].description #get description of one object, the first being the one with the highest confidnce score.    
     if description:
         return description
     else:
@@ -40,21 +39,45 @@ def get_image_from_frame(cap):
     cv2.imshow('frame',frame) #show camera output
     return file
 
+def detect_face(image_path):
+    with io.open(image_path, 'rb') as image_file:
+        content = image_file.read()
+    image = vision.Image(content=content)    
+    response = client.label_detection(image=image)
+    faces = response.face_annotations
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+    print("Faces:")
+    for face in faces:
+        if face:
+            print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+            print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+            print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+        elif face == None:
+            pass
+        else:
+            print("N/A")
+    
+
+
 def start_camera():
     global object_to_find
     os.system('sudo modprobe bcm2835-v4l2') #Force the Raspberry Pi to use the the Picamera, which CV2 will need to capture each frame.
-
     cap = cv2.VideoCapture(0)
+    
+    
+    
     print("Starting camera")
 
+
     while True:
-        
         img = get_image_from_frame(cap)
+        
         key = cv2.waitKey(0) #press 0 to move through frames
         object_to_find = get_object_from_cloud(img)
-
+        finding= detect_face(img)
         print(object_to_find)
-
+        print(finding)
         if key == ord('q'): #press q to quit
             break
     
